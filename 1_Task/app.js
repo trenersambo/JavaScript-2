25b8f5a0e85ed9d737763fb733e9699d592c2c58
trenersambo <arb.pfr@gmail.com>
2022-01-17 19:04:07 +0300

    дом/задание №4_task_1
    Замена ' на "

---
+++ 1_Task/app.js
@@ -0,0 +1,45 @@
+//Решение ДЗ№4 задача№1 - см. строка 15
+//сорян за 'много-воды-в-коде-но-хотел-украсить'
+
+
+function replacement(){
+ 
+const btnReplace = document.querySelector ('.btn-replace') ;
+ 
+btnReplace.addEventListener ('click' , replaceFunc); 
+
+function replaceFunc( ){
+     
+    //Найти все ' и заменить их на  "
+     //console.log (bigWordDiv.replace(/'/g, `"`) )
+
+    const bigWordDiv = (document.querySelector ('.bigWord').textContent).replace(/'/g, `"`);
+ 
+    const bigWordIns = document.querySelector ('.bigWord_ins') ;
+    bigWordIns.innerText = `${bigWordDiv}` ;
+ 
+    hidden( )
+
+    }
+} 
+
+function hidden( ){
+     
+    const btnReplace = document.querySelector ('.btn-replace') ;   
+
+     btnReplace.innerText = "Очистить поле" ;
+
+    btnReplace.addEventListener ('click' , clear);   
+     function clear(){
+     const bigWordIns = document.querySelector ('.bigWord_ins') ;
+     bigWordIns.innerText = `Тут будет замененный текст` ; 
+
+     btnReplace.innerText = "Замена" ;
+     replacement()   
+    }
+
+}
+ 
+replacement()
+
+//17.01.2022г
